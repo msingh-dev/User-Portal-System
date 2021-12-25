06414b747eb606e914555879997232cbc1357775
maingh-dev <singh.manish7@outlook.in>
2021-12-25 12:53:14 +0530

    Initial Commit: JWT + Angular and Spring Project

---
+++ src/test/java/com/user/portal/supportportalapplication/SupportPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.portal.supportportalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
